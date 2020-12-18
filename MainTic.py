9c6ae95d88346c36d3eaa34e864e7e5a70635001
cianoner <67595347+cianoner@users.noreply.github.com>
2020-12-18 21:55:29 +0000

    Update MainTic.py
    Update: User_input function finished. Need to add error handling for cases which the user does not enter correct data

--- MainTic.py
+++ MainTic.py
@@ -1,9 +1,9 @@
 #Import Needed libraries, maybe numPy, random, sys? 
-##
+import sys
 ##Define Board 3x3 
-board =[[0,2,0],
+board =[[0,1,0],
         [0,0,0],
-        [0,1,0]]
+        [0,2,0]]
 
 #defines board numbers as Xs or Os
 def printXO(xo):
@@ -29,3 +29,20 @@ def show_board(thisBoard):
             print("  ----------")
         count+=1
 show_board(board)
+#function that allows user input. passes in the current board and the current player
+def userInput(thisBoard, currentPlayer):
+    print("\t Current Player: ", currentPlayer)
+    
+    row = int(input("Please choose a row to play: "))
+    column = int(input("Please choose a column to play: "))
+
+    if thisBoard[row][column] == 0:
+        thisBoard[row][column] = currentPlayer
+    else:
+        print("Taken! please choose again.")
+        userInput(thisBoard,currentPlayer)
+    
+    userInput(board,1)
+    show_board(board)
+
+    sys.stdin.readline()
