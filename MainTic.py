797189accfa38d35061fdd8336eecdfe87e10994
cianoner <67595347+cianoner@users.noreply.github.com>
2020-12-18 21:18:29 +0000

    Update MainTic.py
    Update: Created def printXO function to replace ints on board with Xs, Os or blank spaces.

--- MainTic.py
+++ MainTic.py
@@ -1,11 +1,18 @@
 #Import Needed libraries, maybe numPy, random, sys? 
 ##
 ##Define Board 3x3 
-board =[[0,0,0],
+board =[[0,2,0],
         [0,0,0],
-        [0,0,0]]
+        [0,1,0]]
 
-
+#defines board numbers as Xs or Os
+def printXO(xo):
+    if xo == 1:
+        return "X"
+    elif xo == 2:
+        return "O"
+    else:
+        return " "
 #Function that prints board to terminal.        
 def show_board(thisBoard):
 #Prints  axis for the board
@@ -15,11 +22,10 @@ def show_board(thisBoard):
     for i in thisBoard:
         in_row = ""
         for j in i[:len(thisBoard)-1]:
-            in_row+=str(j)+" | "
-        in_row+=str(i[len(thisBoard)-1])
+            in_row+=printXO(j)+" | "
+        in_row+=printXO(i[len(thisBoard)-1])
         print(count,in_row)
         if count!=len(thisBoard)-1:
             print("  ----------")
         count+=1
-
 show_board(board)
